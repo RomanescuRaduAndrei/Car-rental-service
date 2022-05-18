ae776c230dde5571c6e471a22ec3d1dbf1b35314
Radu <emaeee@yahoo.com>
2022-05-18 22:32:07 +0300

    Added project Files

---
+++ WebApplication1/WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CarRental.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cars> Cars { get; set; }
+        public DbSet<Rents> Rents { get; set; }
+    }
+}
---
+++ WebApplication1/WebApplication1/Data/Migrations/20220518155243_m4.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Data.Migrations
+{
+    public partial class m4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Model = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    An = table.Column<int>(type: "int", nullable: false),
+                    Firma = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Imagine = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HorsePower = table.Column<int>(type: "int", nullable: false),
+                    Traction = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Transmission = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FuelType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Weight = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RentBegin = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RentEnd = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PayMethod = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rents", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+
+            migrationBuilder.DropTable(
+                name: "Rents");
+        }
+    }
+}
